adca96bc490584f9894f1fd8f9537c920664da85
Liwei Yang <yangliwei.uestc@gmail.com>
2017-03-12 17:47:14 +0800

    add solution: SortingArray.cpp

--- DRW/NumSquares.cpp
+++ DRW/NumSquares.cpp
@@ -1,7 +1,34 @@
 // Question:
 // Write a function that gets the number of squares between two numbers
 
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
 int main() {
+  unsigned A, B;
+  unsigned NumSq = 0;
+
+  cout << "A: ";
+  cin >> A;
+  cout << "B: ";
+  cin >> B;
+
+  double SqrtA = sqrt(A);
+  double SqrtB = sqrt(B);
+
+  for (unsigned i = A; i <= B; i++) {
+    for (unsigned j = SqrtA; j <= SqrtB + 1; j++) {
+      if ((j^2) == i) {
+        cout << "found square: " << i << "\n";
+        NumSq++;
+        break;
+      }
+    }
+  }
+
+  cout << "total num of squares: " << NumSq << "\n";
 
   return 0;
 }
