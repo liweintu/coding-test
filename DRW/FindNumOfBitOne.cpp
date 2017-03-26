a0d7af59ea97475c38a6455ee45c0423973d2f4b
yangliwei <yangliwei.uestc@gmail.com>
2017-03-26 15:19:11 +0800

    finish FindNumOfBitOne

--- DRW/FindNumOfBitOne.cpp
+++ DRW/FindNumOfBitOne.cpp
@@ -2,7 +2,37 @@
 // Given an integer if that int was represented in binary how many bits
 // would be set to '1'? (no time or space complexity requirements only accuracy) 
 
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <stdlib.h>
+
+using namespace std;
+
+#define BITWIDTH 32
+
+int FindNumOfBitOne(int In) {
+  string InStr = bitset<BITWIDTH>(In).to_string();
+  cout << "Binary format: " << InStr << "\n";
+  const char *InChars = InStr.c_str();
+
+  unsigned NumOfBitOne = 0;
+
+  for (unsigned i = 0; i < BITWIDTH; i++) {
+    if (InChars[i] == '1') {
+      NumOfBitOne++;
+    }
+  }
+
+  return NumOfBitOne;
+}
+
 int main() {
+  int In;
+  cout << "Enter an integer: ";
+  cin >> In;
+
+  cout << "Num of ones: " << FindNumOfBitOne(In) << "\n";
 
   return 0;
 }
