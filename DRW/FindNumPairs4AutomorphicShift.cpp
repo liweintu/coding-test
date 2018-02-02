21ae58abdb05e32c1114a4f7031e083e8df5d784
yangliwei <yangliwei.uestc@gmail.com>
2018-02-03 02:52:20 +0800

    finished FindNumPairs4AutomorphicShift

--- DRW/FindNumPairs4AutomorphicShift.cpp
+++ DRW/FindNumPairs4AutomorphicShift.cpp
@@ -1,22 +1,89 @@
 // Question:
-// calculating the number of the sets of points that are collinear
+// calculating the pairs that contribute to the automorphic cyclic shift
+// given a matrix
 
 #include <iostream>
-#include <math.h>
-#include <stdlib.h>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int solution(int **Matrix, int Rows, int Cols) {
+  int OldMatrix[Rows][Cols];
+  int NewMatrix[Rows][Cols];
+  int NumInCol1 = 0;
+  int NumInRow1 = 0;
 
+  // initialization
+  for (int j = 0; j < Rows; j++) {
+    for (int k = 0; k < Cols; k++) {
+      OldMatrix[j][k] = Matrix[j][k];
+      NewMatrix[j][k] = Matrix[j][k];
+    }
+  }
 
+  // shift right
+  for (int i = 0; i < Cols - 1; i++) {
+    for (int j = 0; j < Rows; j++) {
+      for (int k = 0; k < Cols; k++) {
+        NewMatrix[j][(k + 1) % Cols] = OldMatrix[j][k];
+      }
+    }
+
+    bool IsSame = true;
+    for (int j = 0; j < Rows; j++) {
+      for (int k = 0; k < Cols; k++) {
+        IsSame &= NewMatrix[j][k] == Matrix[j][k];
+        OldMatrix[j][k] = NewMatrix[j][k];
+      }
+    }
+
+    // if (memcmp(NewMatrix, Matrix, sizeof(int) * Rows * Cols) == 0)
+    if (IsSame)
+      NumInRow1++;
+  }
+
+  // re-initialize NewMatrix
+  for (int j = 0; j < Rows; j++) {
+    for (int k = 0; k < Cols; k++) {
+      OldMatrix[j][k] = Matrix[j][k];
+      NewMatrix[j][k] = Matrix[j][k];
+    }
+
+  }
+  // shift down
+  for (int i = 0; i < Rows - 1; i++) {
+    for (int j = 0; j < Rows; j++) {
+      for (int k = 0; k < Cols; k++) {
+        NewMatrix[(j + 1) % Rows][k] = OldMatrix[j][k];
+      }
+    }
+
+    bool IsSame = true;
+    for (int j = 0; j < Rows; j++) {
+      for (int k = 0; k < Cols; k++) {
+        IsSame &= NewMatrix[j][k] == Matrix[j][k];
+        OldMatrix[j][k] = NewMatrix[j][k];
+      }
+    }
+
+    // if (memcmp(NewMatrix, Matrix, sizeof(int) * Rows * Cols) == 0)
+    if (IsSame)
+      NumInCol1++;
+  }
+
+  cout << "NumInRow1: " << NumInRow1 << endl;
+  cout << "NumInCol1: " << NumInCol1 << endl;
+  cout << "#pairs: " << NumInRow1 + NumInCol1 + NumInRow1 * NumInCol1 + 1 << endl;
   return 0;
 }
 
 
 int main() {
-  int Rows = 4;
-  int Cols = 4;
+  int Rows = 8;
+  int Cols = 8;
   int **Matrix;
   Matrix = new int *[Rows];
 
