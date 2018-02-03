f607f0bd318e1ff6fe6d3759bf4deda19e3b1f67
yangliwei <yangliwei.uestc@gmail.com>
2018-02-03 12:59:37 +0800

    finished a better solution for Roots2Coeffs

--- DRW/Roots2Coeffs.cpp
+++ DRW/Roots2Coeffs.cpp
@@ -9,38 +9,46 @@
 using namespace std;
 
 int solution(vector<int> &A) {
-  int Sum = 0;
-  vector<int> Coeffs;
 
-  // initialize the coefficients to 0s
-  for (unsigned i = 0; i <= A.size(); i++) {
-    Coeffs.push_back(0);
+  // solution 1 (bad):
+  // int Sum = 0;
+  // vector<int> Coeffs;
+
+  // // initialize the coefficients to 0s
+  // for (unsigned i = 0; i <= A.size(); i++) {
+  //   Coeffs.push_back(0);
+  // }
+  //
+  // // initilize the coefficients for the first Root
+  // Coeffs[0] = A[0];
+  // Coeffs[1] = 1;
+
+  // for (unsigned j = 1; j < A.size(); j++) {
+  //   int Root = A[j];
+  //   cout << "Root: " << Root << endl;
+  //   Coeffs[j + 1] = 1;
+
+  //   for (unsigned k = j; k > 0; k--) {
+  //     Coeffs[k] = Root * Coeffs[k] + Coeffs[k - 1];
+  //   }
+  //   Coeffs[0] *= Root;
+  // }
+
+  // for (unsigned l = 0; l < Coeffs.size(); l++) {
+  //   cout << Coeffs[l] << " x^" << l << endl;
+  //   Sum += Coeffs[l];
+  // }
+
+  // solution 2 (good): the sum of coefficients is just the value of the
+  // polynomial when X = 1, i.e. value = (1+Root1) * (1+Root2) * ... * (1+RootN)
+
+  int Products = 1;
+  for (int Root : A) {
+    Products *= (Root + 1);
   }
-  
-  // initilize the coefficients for the first Root
-  Coeffs[0] = A[0];
-  Coeffs[1] = 1;
 
-  for (unsigned j = 1; j < A.size(); j++) {
-    int Root = A[j];
-    cout << "Root: " << Root << endl;
-    Coeffs[j + 1] = 1;
-
-    for (unsigned k = j; k > 0; k--) {
-      Coeffs[k] = Root * Coeffs[k] + Coeffs[k - 1];
-    }
-    Coeffs[0] *= Root;
-  }
-
-  for (unsigned l = 0; l < Coeffs.size(); l++) {
-    cout << Coeffs[l] << " x^" << l << endl;
-    Sum += Coeffs[l];
-  }
-
-  // cout << "Coeffs size(): " << Coeffs.size() << endl;
-  cout << "Sum: " << Sum << endl;
-
-  return Sum;
+  cout << "Products: " << Products << endl;
+  return Products;
 }
 
 int main() {
@@ -50,9 +58,9 @@ int main() {
   A.push_back(-4);
   A.push_back(4);
 
-  solution(A);
+  int Sum = solution(A);
 
-  cout << "solution returns: " << endl;
+  cout << "solution returns: " << Sum << endl;
 
   return 0;
 }
